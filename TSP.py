b9802364cd511f6be1fe18d2a126e96d900fd3d2
Atlas666 <544871201@qq.com>
2018-11-11 23:45:18 +0800

    commit

--- TSP.py
+++ TSP.py
@@ -3,32 +3,46 @@ from Ant import *
 import numpy as np
 
 
-class AntColonySystem:
+class ACS:
     def __init__(self,
                  num_ant=10,
                  w_heuristic=2,
                  w_pheromone_vapor=0.1,
                  q0=0.9,
-                 p=0.1):
-        self.__m = num_ant               # 蚂蚁数目
-        self.__b = w_heuristic           # 启发式信息权重
-        self.__a = w_pheromone_vapor     # 信息素挥发因子
-        self.__t0 = 0                    # 初始信息素
-        self.__q0 = q0                   # 伪随机因子
-        self.__p = p                     # 信息素局部挥发因子
+                 p=0.1,
+                 max_gen=2000,
+                 city_name=""
+                 ):
 
-        self.__ant = []
+        self.__m = num_ant                                      # 蚂蚁数目
+        self.__b = w_heuristic                                  # 启发式信息权重
+        self.__a = w_pheromone_vapor                            # 信息素挥发因子
+        self.__t0 = 0                                           # 初始信息素
+        self.__q0 = q0                                          # 伪随机因子
+        self.__p = p                                            # 信息素局部挥发因子
+        self.__gen = max_gen                                    # 最大进化代数
+
+        self.__ant = np.array(self.__m)                         # 蚁群
+        self.__city = City(city_name)                           # 城市对象
+        self.__dis_matrix = self.__city.get_dis_matrix()        # 距离矩阵
+        self.__pheromone_matrix = self.__dis_matrix             # 信息素矩阵
 
     def init(self):
         # 初始化所有蚂蚁
         for i in range(self.__m):
-            self.__ant.append(Ant())
+            self.__ant[i] = Ant()
+
+        # 信息素初始化
 
     def path_construct(self):
         pass
 
-    def pheromone_global_update(self):
+    def pheromone_update(self):
         pass
 
     def run_acs(self):
+        self.init()
+        for i in range(self.__gen):
+            self.path_construct()
+            self.pheromone_update()
         pass
